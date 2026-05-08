92793eda1be45e037ad175a53fcc7e8342f9a6fc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-08 10:55:14 -0300

    refactor: Layout WIP da logica script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,28 @@
-let nomeVinho = prompt("Digite o nome do vinho:");
-alert(`Nome do Vinho já está disponível no console!`);
+vinhosCadastrados = 0;
+vinhosEstoqueBaixo = 0;
 
-// Console.log onde exibe o nome do vinho selecionado
-console.log("Nome do Vinho: " + nomeVinho);
+function verificarVinho(nome, quantidade, idade) {
+    if (quantidade < 5) {
+        vinhosEstoqueBaixo++;
+    }
 
-let tipoVinho = prompt("Digite o tipo do vinho: Tinto, Branco, Rosé");
-alert(`Tipo de Vinho já está disponível no console!`);
+    vinhoClassificacao = "";
 
-// Console.log onde exibe o tipo do vinho selecionado
-console.log("Tipo do Vinho: " + tipoVinho);
+    if (idade < 2) {
+    //  vinhoClassificao = "Jovem"
+    } else if (idade > 3) {
+    // vinhoClassificao = "Jovem"
+    } else if (idade > 6) {
+    // vinhoClassificao = "Jovem"
+    }
 
-let safraVinho = prompt("Digite a safra do vinho:");
-alert(`Safra do Vinho já está disponível no console!`);
+    alert(`O vinho ${nome} é classificado como ${vinhoClassificacao}`)
+}
 
-// Console.log onde exibe o safra do vinho
-console.log("Safra do Vinho: " + safraVinho);
-
-let precoVinho = parseFloat(prompt("Digite o preço do vinho:"));
-alert(`Preço do Vinho já está disponível no console!`);
-
-// Console.log para a variavel " precoVinho"
-console.log("Preço: R$ " + precoVinho.toFixed(2));
-
-let quantidadeVinho = parseInt(prompt("Digite a quantidade de garrafas no estoque:"));
-alert(`Quantidade no Estoque já está disponível no console!`);
-
-// Console.log para a variavel " quantidadeVinho"
-console.log("Quantidade no Estoque: " + quantidadeVinho);
-
-alert("O Cadastro foi realizado com sucesso! Veja mais detalhes no console.");
+while (true) {
+    nomeVinho = prompt("Cadastre o nome do vinho: ");
+    quantidadeVinho = prompt("Insira a quantidade de vinho(s): ");
+    idadeVinho = prompt("Insira a idade do vinho: ");
 
 
-// Alert para a descrição do cadastro 
-alert(`Descrição do Cadastro:\n\n Nome do vinho: ${nomeVinho}\n Tipo de Vinho: ${tipoVinho}\n Safra: ${safraVinho}\n Preço: R$ ${precoVinho}\n Quantidade no Estoque: ${quantidadeVinho}`);
+}
